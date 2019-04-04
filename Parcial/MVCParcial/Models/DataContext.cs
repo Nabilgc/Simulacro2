09ea30f8da16978754c3338cd76d940459580a49
Nabil Galeb <nabilgaleb@hotmail.com>
2019-04-04 00:31:08 -0400

    MVC MVCModel DataContext

---
+++ Parcial/MVCParcial/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace MVCParcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base ("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Parcial/MVCParcial/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCParcial.Models
+{
+    public enum UnityType
+    {
+        Botella,
+        Litro,
+        Metro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Name { get; set; } 
+        
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Required]
+
+        public UnityType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
