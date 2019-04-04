ce7447a6d7f9408a58b3522edb887f311642ea04
Nabil Galeb <nabilgaleb@hotmail.com>
2019-04-04 01:04:28 -0400

    Modelo WebAPI

---
+++ Parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIparcial.Models
+{
+    public enum UnityType
+    {
+        Botella,
+        Litro,
+        Metro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Required]
+
+        public UnityType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
